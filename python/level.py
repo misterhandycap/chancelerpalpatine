2713731e5bc95ba7d2241c0bdb025e00859e9ba0
Thales César <thalesgiriboni@gmail.com>
2020-09-04 17:57:23 -0300

    Fix on_message command processing

--- python/level.py
+++ python/level.py
@@ -26,6 +26,8 @@ async def on_message(message):
 
     with open('users.json', 'w') as f:
         json.dump(users, f)
+    
+    await client.process_commands(message)
 
 async def update_data(users, user):
     if not user.id in users:
