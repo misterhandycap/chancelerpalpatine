7c12e3ce2ba2ab0be9060a8e0a20fb4e59b47dc2
Thales César <thalesgiriboni@gmail.com>
2020-09-04 21:04:30 -0300

    Fix duplicate user entry

--- python/level.py
+++ python/level.py
@@ -30,20 +30,20 @@ async def on_message(message):
     await client.process_commands(message)
 
 async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experiencia'] = 0
-        users[user.id]['level'] = 1
+    if not str(user.id) in users:
+        users[str(user.id)] = {}
+        users[str(user.id)]['experiencia'] = 0
+        users[str(user.id)]['level'] = 1
 
 async def add_xp(users, user, xp):
-    users[user.id]['experiencia'] += xp
+    users[str(user.id)]['experiencia'] += xp
 
 async def level_up(users, user, channel):
-    experiencia = users[user.id]['experiencia']
-    level_start = users[user.id]['level']
+    experiencia = users[str(user.id)]['experiencia']
+    level_start = users[str(user.id)]['level']
     level_end = int(experiencia **(1/4))
 
     if level_start < level_end:
         await client.send_message(channel, '{} se tornou mais valioso ao subir ao nível {}'.format(user.mention, level_end))
-        users[user.id]['level'] = level_end
+        users[str(user.id)]['level'] = level_end
         await client.process_commands(message)
