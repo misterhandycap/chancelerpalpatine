6752c211b78af27cecdcd12e2f9f3b7448e170b2
Rise <68623862+misterhandycap@users.noreply.github.com>
2020-09-06 21:04:01 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,52 @@
+import discord
+import random
+import json
+import os
+from discord.ext import commands
+from bot import client
+
+@client.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(f'Planejando uma ordem surpresa'))
+    print('É bom te ver, mestre Jedi.')
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredPermission):
+        await ('Parece que você não tem poder aqui, Jedi, cheque com os mestres do Conselho e volte mais tarde.')
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Esta ordem não existe, agora se me der licença...')
+
+@client.command()
+async def ping(ctx):
+    ping = discord.Embed(title='Pong...', description=f'{round(client.latency * 1000)}ms', colour=discord.Color.blurple())
+    await ctx.send(embed=ping)
+
+@client.command(aliases=['limpar', 'clean'])
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=5+1):
+    await ctx.channel.purge(limit=amount)
+
+@client.command(aliases=['8ball'])
+async def vision(ctx, *, question):
+    responses = ['Assim é.', 'Está me ameaçando?', 'É certo.', 'Acho que devemos buscar mais informações.', 'Isso não está correto.', 'Você está errado.', 'Não, não, NÃO!!', 'Acredito que esteja errado, Mestre', 'Isso necessita de mais análises']
+    await ctx.send(f'{random.choice(responses)}')
+
+@vision.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Queria me perguntar algo, Jedi?')
+
+@client.command()
+async def sorte(ctx):
+    previsao = ['Cara', 'Coroa']
+    await ctx.send(f'{random.choice(previsao)}')
+
+#os.system('python level.py')
