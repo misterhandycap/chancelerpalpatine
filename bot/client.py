62e1efa5ec33efac9e9efd66bf476089fd39c24f
Thales César <thalesgiriboni@gmail.com>
2020-11-05 21:32:46 -0300

    Add env var for optional astrology bot disable

--- bot/client.py
+++ bot/client.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from bot.akinator.akinator_game import AkinatorGame
-from bot.astrology.astrology_chart import AstrologyChart
 from bot.chess.chess import Chess
 from bot.chess.puzzle import Puzzle
 from bot.sww_leaderboard.leaderboard import Leaderboard
@@ -19,8 +18,11 @@ client = commands.Bot(command_prefix=os.environ.get("BOT_PREFIX", 'cp!'))
 chess_bot = Chess()
 chess_bot.load_games()
 
-astrology_bot = AstrologyChart()
-astrology_bot.load_charts()
+astrology_bot = None
+if os.environ.get("DISABLE_ASTROLOGY") not in ['True', 'true']:
+    from bot.astrology.astrology_chart import AstrologyChart
+    astrology_bot = AstrologyChart()
+    astrology_bot.load_charts()
 
 puzzle_bot = Puzzle()
 
