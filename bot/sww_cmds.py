a316a87582d7cb62943f3b9fae504d4dfcebcee5
Thales César <thalesgiriboni@gmail.com>
2020-12-11 17:40:49 -0300

    Add embed to medals cmd

--- bot/sww_cmds.py
+++ bot/sww_cmds.py
@@ -17,21 +17,18 @@ async def leaderboard(ctx, page=1):
         logging.warning(e, exc_info=True)
         return await ctx.send("Houve um erro ao obter o quadro de lideranças da Star Wars Wiki.")
 
-@client.command(aliases=['medalha', 'medalhas', 'medal'])
-async def medals(ctx, medal_name=None):
+@client.command(aliases=['medalha'])
+async def medal(ctx, *args):
+    medal_name = ' '.join(args)
     await ctx.trigger_typing()
     try:
         leaderboard_data = await leaderboard_bot.get()
         medals = await leaderboard_bot.build_medals_info(*leaderboard_data)
-        if not medal_name:
-            final_text = "Essas são as medalhas disponíveis na Star Wars Wiki:\n```"
-            for medal_info in medals:
-                final_text += f"* {medal_info['name']}\n"
-            final_text += "```\n"
-            return await ctx.send(final_text)
+        
         medal_info = [medal for medal in medals if medal['name'] == medal_name]
         if not medal_info:
             return await ctx.send("Medalha não encontrada")
+        
         medal_info = medal_info[0]
         embed = discord.Embed(
             title='Medalhas da Star Wars Wiki',
@@ -43,6 +40,34 @@ async def medals(ctx, medal_name=None):
         embed.add_field(name='Descrição', value=medal_info['text'])
         embed.add_field(name='Pontos', value=medal_info['points'])
         await ctx.send(embed=embed)
+    except Exception as e:
+        logging.warning(e, exc_info=True)
+        return await ctx.send("Houve um erro ao obter o quadro de lideranças da Star Wars Wiki.")
+
+@client.command(aliases=['medalhas'])
+async def medals(ctx, page: int=1):
+    max_medals_per_page = 6
+    await ctx.trigger_typing()
+    if page < 0:
+        page = 0
+    try:
+        leaderboard_data = await leaderboard_bot.get()
+        medals = await leaderboard_bot.build_medals_info(*leaderboard_data)
+        interval_start = (page-1) * max_medals_per_page
+        interval_end = page * max_medals_per_page
+        last_page = len(medals) // max_medals_per_page + (len(medals) % max_medals_per_page > 0)
+        if page > last_page:
+            return await ctx.send("Sem mais medalhas")
+        
+        embed = discord.Embed(
+            title='Medalhas da Star Wars Wiki',
+            description=f'Página {page}/{last_page}',
+            colour=discord.Color.blurple(),
+            timestamp=ctx.message.created_at
+        )
+        for medal_info in medals[interval_start:interval_end]:
+            embed.add_field(name=medal_info['name'], value=medal_info['text'])
+        return await ctx.send(embed=embed)
     except Exception as e:
         logging.warning(e, exc_info=True)
         return await ctx.send("Houve um erro ao obter o quadro de lideranças da Star Wars Wiki.")
