20373686f43f9728cd5659ab1569bd9f3f83d64f
Rise <cauaciconelli@gmail.com>
2021-02-18 20:43:59 -0200

    Inicio de Across The Stars!!!!!!

---
+++ bot/models/migrations/versions/b574977c1728_create_planet_table.py
@@ -0,0 +1,37 @@
+"""create planet table
+
+Revision ID: b574977c1728
+Revises: 6c157a54a570
+Create Date: 2021-02-17 22:18:29.501892
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+from bot.models.guid import GUID
+from uuid import uuid4
+
+# revision identifiers, used by Alembic.
+revision = 'b574977c1728'
+down_revision = '6c157a54a570'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'planet',
+        sa.Column('id', GUID(), primary_key=True, default=uuid4),
+        sa.Column('name', sa.String, nullable=False),
+        sa.Column('happines_base', sa.Integer, nullable=False),
+        sa.Column('importance_base', sa.Integer, nullable=False),
+        sa.Column('price', sa.Integer, nullable=False),
+        sa.Column('region', sa.String, nullable=False),
+        sa.Column('climate', sa.String, nullable=False),
+        sa.Column('circuference', sa.Integer, nullable=False),
+        sa.Column('mass', sa.Integer, nullable=False),
+    )
+
+def downgrade():
+    op.drop_table('planet')
---
+++ bot/models/planets.py
@@ -0,0 +1,24 @@
+from sqlalchemy import BigInteger, Column, DateTime, Integer, select, String
+from bot.models import engine, Base
+from bot.models.guid import GUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from uuid import uuid4
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(GUID(), primary_key=True, default=uuid4)
+    name = Column(String, nullable=False)
+    happines_base = Column(Integer, nullable=False)
+    importance_base = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+    region = Column(String, nullable=False)
+    climate = Column(String, nullable=False)
+    circuference = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+
+    @classmethod
+    async def all(cls, region=None, climate=None, circuference=None, mass=None):
+        async with AsyncSession(engine) as session:
+            return (await session.execute(
+                select(Planet).filter_by(region=region) #, climate=climate, circuference=circuference, mass=mass) 
+            )).scalars().fetchall()
