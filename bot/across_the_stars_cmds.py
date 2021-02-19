f3e337d76884de2e20efc0510060d99a3c8d33c2
Rise <cauaciconelli@gmail.com>
2021-02-18 23:49:31 -0200

    embed em Across the Stars

--- bot/across_the_stars_cmds.py
+++ bot/across_the_stars_cmds.py
@@ -12,6 +12,25 @@ class AcrossTheStarsCmds(commands.Cog):
     @commands.command(aliases=['planets'])
     async def planetas(self, ctx, *, args):
         planets = await self.planets.list_of_planets(region=args)
-        print(planets)
-        await ctx.reply('aoba'
+        
+    discord_file = discord.File(
+            os.path.join('bot', 'images', 'arnaldo-o-hutt.gif'), 'hutt.gif')
+        await self.shop_paginated_embed_manager.send_embed(
+            await self._build_shop_embed(page_number), page_number,
+            ctx, discord_file
+        )
+
+        embed = discord.Embed(
+            title='Empório do Arnaldo',
+            description='Se torne senador em nome de um planeta.',
+            colour=discord.Color.green()
+        )
+        embed.set_thumbnail(url="attachment://hutt.gif")
+        self.shop_paginated_embed_manager.last_page = last_page
+        for planet in planets:
+            embed.add_field(
+                name=profile_item.name,
+                value=f'Preço: {profile_item.price}\nTipo: {profile_item.type.name.capitalize()}'
+
+        await ctx.reply(embed=embed
         mention_author=False)
